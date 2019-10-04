28cb5697ed7ef8a40e3f76e28bf2c2fc255b2b0e
Todd Barchok <todd@barchok.com>
2019-10-04 15:15:07 -0400

    First attempt at parsing

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Dice Rolling Program</title>
 <style type="text/css">
 body {
-	margin: 0 30%;
+	margin: 20px 30%;
 }
 </style>
 </head>
@@ -12,10 +12,74 @@ body {
 <input name="dice_string" type="text" value="2d6 + 3d8" />
 <input type="submit" value="Roll!" />
 </form>
+<br><hr><br>
+<form method="POST">
+<input name="tests" type="hidden" value="true" />
+<input type="submit" value="Run Tests!" />
+</form>
 <?php
+function split_into_array($s)
+{
+	$array = [];
+	$opers = [];
+	/*$next = 0;
+	$sign = "";
+	do
+	{
+		$plus = stripos($s, "+");
+		$neg = stripos($s, "-");
+		if(($plus !== FALSE && $neg !== FALSE && $plus < $neg) || ($plus !== FALSE && $neg === FALSE))
+		{
+			$next = $plus - 1;
+			$sign = "+";
+		} else if(($neg !== FALSE && $plus !== FALSE && $neg < $plus) || ($neg !== FALSE && $plus === FALSE))
+		{
+			$next = $neg - 1;
+			$sign = "-";
+		} else {
+			$next = strlen($s);
+			$sign = "";
+		}
+		array_push($array, trim(substr($s, 0, $next)));
+		if(!empty($sign))
+		{
+			array_push($array, $sign);
+		}
+		if($next > 0)
+		{
+			$s = substr($s, $next + 2);
+		}
+	} while(strlen($s) > 0);*/
+	if(substr_count($s, "+"))
+	{
+		$array = explode("+", $s);
+		array_fill($opers, count($array - 1), "+");
+	}
+	if(substr_count($s, "-"))
+	{
+		$array = explode("-", $s);
+		array_fill($opers, count($array - 1), "-");
+	}
+	return $array;
+}
+
+function process_roll($input)
+{
+	$die_list = split_into_array($input);
+	var_dump($die_list);
+}
+
 if(isset($_POST['dice_string']))
 {
 	echo "<br>You rolled ".$_POST['dice_string'];
+}
+
+if(isset($_POST['tests']))
+{
+	process_roll("2d6 + 3d8");
+	process_roll("d6 + 3");
+	process_roll("d6 + 1d12 + 2d100");
+	process_roll("d6 - 1d12 + 2d100");
 }
 /*Write a program that calculates a dice addition equation.
 
