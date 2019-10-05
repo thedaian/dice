c8bafad6c84af8ff697cd73f090bb40da5f54493
Todd Barchok <todd@barchok.com>
2019-10-05 15:37:13 -0400

    Added dice database sql file

---
+++ rolls.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 05, 2019 at 07:36 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dice`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rolls`
+--
+
+DROP TABLE IF EXISTS `rolls`;
+CREATE TABLE IF NOT EXISTS `rolls` (
+  `time` timestamp NOT NULL,
+  `raw` varchar(255) NOT NULL,
+  `string_result` varchar(512) NOT NULL,
+  `final_number` int(11) NOT NULL,
+  PRIMARY KEY (`time`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
